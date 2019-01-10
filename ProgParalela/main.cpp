2c88a89fc1248aa96247ae5b977ee3cd620edb1d
Nacho Martínez <anxooxna@me.com>
2019-01-10 20:16:41 +0100

    Initial Commit

---
+++ ProgParalela/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  ProgParalela
+//
+//  Created by Nacho Martinez on 10/01/2019.
+//  Copyright © 2019 Nacho Martinez. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
